0550c15b1488905c333ab0d9268d89b9a89b2ba9
jeongsooyoung <code.ameba@gmail.com>
2020-06-04 17:11:18 +0900

    github link target _blank

--- src/components/header.js
+++ src/components/header.js
@@ -78,7 +78,7 @@ const Header = ({ siteTitle }) => (
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
         <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/codeAmeba">
+        <GitHubLink target="_blank" href="https://github.com/codeAmeba">
           GitHub
         </GitHubLink>
       </p>
